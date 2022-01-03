afa10192aa260d828cb2ecbd7a806e45397df121
ishrafil2233 <ishrafil2233@gmail.com>
2022-01-03 22:24:51 +0600

    add payment method

--- src/Components/Dashboard/Payment/Payment.js
+++ src/Components/Dashboard/Payment/Payment.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { useParams } from 'react-router-dom';
+
+const stripePromise = loadStripe('pk_test_51KDs8DIHpykq4r7Xg1IymHWlM6HyZXpnlCw89tGLd5Bu0wQTvLabKFFj8l9weFZBa86ASjHnZiJqLlzBl7PruJRe00v0iPcpAi')
 
 const Payment = () => {
+    const { orderId } = useParams();
+    const [order, setOrder] = useState({});
+    useEffect(() => {
+        fetch(`https:/${orderId}`)
+            .then(res => res.json())
+            .then(data => setOrder(data));
+    }, [orderId]);
     return (
         <div>
-            <h4>Payment page</h4>
-            <h3> payment</h3>
+            {/* <h2>Please Pay for: {appointment.patientName} for {appointment.serviceName}</h2>
+            <h4>Pay: ${appointment.price}</h4> */}
+            {order?.price && <Elements stripe={stripePromise}>
+                <CheckoutForm
+                    order={order}
+                />
+            </Elements>}
         </div>
     );
 };
